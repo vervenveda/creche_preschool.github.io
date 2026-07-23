ae2352106a6f2e66421b25f3764ef8f487e08448
vervenveda <vervenveda@proton.me>
2026-07-22 20:14:00 -0400

    Add pronouns data structure

---
+++ assets/pronouns.js
@@ -0,0 +1,23 @@
+const PRONOUNS = [
+  {
+    subject: "he",
+    object: "him",
+    possessive: "his",
+    possessivePronoun: "his",
+    reflexive: "himself"
+  },
+  {
+    subject: "she",
+    object: "her",
+    possessive: "her",
+    possessivePronoun: "hers",
+    reflexive: "herself"
+  },
+  {
+    subject: "they",
+    object: "them",
+    possessive: "their",
+    possessivePronoun: "theirs",
+    reflexive: "themselves"
+  }
+];
