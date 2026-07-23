fc6edfca24bc04388b5e5e0ed7032d3eac925d7d
vervenveda <vervenveda@proton.me>
2026-07-22 20:14:28 -0400

    Add identities.js with gender identity objects

---
+++ assets/identities.js
@@ -0,0 +1,23 @@
+const IDENTITIES = [
+  {
+    label: "boy",
+    subject: "he",
+    object: "him",
+    possessive: "his",
+    reflexive: "himself"
+  },
+  {
+    label: "girl",
+    subject: "she",
+    object: "her",
+    possessive: "her",
+    reflexive: "herself"
+  },
+  {
+    label: "friend",
+    subject: "they",
+    object: "them",
+    possessive: "their",
+    reflexive: "themselves"
+  }
+];
